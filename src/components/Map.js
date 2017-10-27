4d4a8f22b370201c0c7cf518a8965bab6c1450f6
David Morais <davidmorais92@gmail.com>
2017-10-28 00:18:33 +0100

    Add Modal component on marker click
    Add Modal style
    Change coordinates structure and name

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import positions from '../dev/positions.json';
+import { Map, Marker, TileLayer } from 'react-leaflet';
+import Modal from 'react-modal';
+import coordinates from '../dev/coordinates.json';
+
+
+const modalContentStyle = {
+  position: 'absolute',
+  backgroundColor: 'orange',
+};
+
+const modalOverlayStyle = {
+  position: 'absolute',
+  zIndex: 1000,
+};
 
 
 export default class LeafletMap extends Component {
@@ -8,33 +20,56 @@ export default class LeafletMap extends Component {
     super(props);
     this.state = {
       zoom: 4,
+      modalOpen: false,
     };
-    this.positions = positions;
+    this.coordinates = coordinates;
+  }
+  componentDidMount() {
+    Modal.setAppElement('.LeafletMap');
   }
 
   pinMarkers() {
     const markersArray = [];
-    const districtPositions = this.positions.districtPositions;
-    for (const coordinates of districtPositions) { // eslint-disable-line
+    const districtPositions = this.coordinates.districts;
+    for (let i = 0; i < districtPositions.length; i++) { // eslint-disable-line
+      const latLong = districtPositions[i].coordinates;
       markersArray.push(
-        <Marker position={coordinates} key={districtPositions.indexOf(coordinates)}s>
-          <Popup>
-            <span>A pretty CSS3 popup. <br /> Easily customizable.</span>
-          </Popup>
-        </Marker>,
+        <Marker
+          position={latLong}
+          key={i}
+          onClick={() => { this.setState({ modalOpen: !this.state.modalOpen }); }}
+        />,
       );
     }
     return markersArray;
   }
+
   render() {
     return (
-      <Map center={this.positions.focusPosition} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
-        {this.pinMarkers()}
-      </Map>
+      <div className="LeafletMap">
+        <Map center={this.coordinates.initialFocus} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          />
+          {this.pinMarkers()}
+        </Map>
+        <Modal
+          isOpen={this.state.modalOpen}
+          onAfterOpen={() => { this.setState({ modalOpen: true }); }}
+          onRequestClose={() => { this.setState({ modalOpen: false }); }}
+          /*
+          closeTimeoutMS={n}
+          */
+          style={{
+            overlay: modalOverlayStyle,
+            content: modalContentStyle,
+          }}
+          contentLabel="Modal"
+        >
+          <h1>Hey Im a modal</h1>
+        </Modal>
+      </div>
     );
   }
 }
